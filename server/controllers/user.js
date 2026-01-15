6d97dcc2937394d14e75d37614297f36e06c98f7
Rajnish Kumar <rajnishmaurya@Rajnishs-MacBook-Air.local>
2026-01-15 14:34:37 +0530

    course purchase bug fix

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -117,7 +117,7 @@ export const purchaseCourse = async (req, res) => {
       courseId,
       userId,
       amount,
-      status: "created",
+      status: "pending",
     });
 
     const order = await razorpayInstance.orders.create({
